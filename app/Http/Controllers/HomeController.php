1b75beb037a87164b67ff8714c2bbe7d60133f20
vuhuyhoang310100 <vuhuyhoang310199@gmail.com>
2024-07-25 11:44:46 +0700

    ui/admin

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Author;
+use App\Models\Book;
+use App\Models\Category;
+use App\Models\Loan;
+use App\Models\Publisher;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index() {
-        return view('admin.dashboard');
+    public function index()
+    {
+        $countBooks = Book::count();
+        $countAuthors = Author::count();
+        $countPublishers = Publisher::count();
+        $countLoans = Loan::count();
+        $countCategories = Category::count();
+        return view('admin.dashboard', compact('countBooks', 'countAuthors', 'countPublishers', 'countLoans', 'countCategories'));
     }
 }
